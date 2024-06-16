67a1c49c85af6f0fc3f8210f99f2ce09fced9462
= <=>
2024-06-16 11:01:30 +0700

    .

--- cocos-js/spine.js-6d0b02e3.js
+++ cocos-js/spine.js-6d0b02e3.js
@@ -1,10 +1,11 @@
-System.register([], (function (exports, module) {
-	'use strict';
-	return {
-		execute: (function () {
-
-			var spine_js = exports('default', 'assets/spine.js.mem-90a4faeb.bin'); /* asset-hash:afa7b80c */
-
-		})
-	};
-}));
+System.register([], function (exports, module) {
+  'use strict'
+  return {
+    execute: function () {
+      var spine_js = exports(
+        'default',
+        'assets/spine.js.mem-90a4faeb.bin'
+      ) /* asset-hash:afa7b80c */
+    }
+  }
+})
